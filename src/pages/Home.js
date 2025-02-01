a969861aea2d22537764c08e303769b1cd1b909c
Jefferson Lima <jefferson@araguaiasistemas.com.br>
2025-02-01 15:13:17 -0300

    Refatorando com Hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
-import { useContext } from "react"; // Import useContext from react
-import { CounterContext } from "../context/CounterContext"; // Import your context
+//import { useContext } from "react"; // Import useContext from react
+//import { CounterContext } from "../context/CounterContext"; // Import your context
+
 import { ChangeCounter } from "../components/ChangeCounter";
 
+// 4 - Refatorando com Hook
+import { useCounterContext } from "../hooks/useCounterContext";
+
 const Home = () => {
-  const { counter } = useContext(CounterContext); // Use useContext to access the context value
+  // const { counter } = useContext(CounterContext); // Use useContext to access the context value
+  const { counter } = useCounterContext();
+
   return (
     <div>
       <h1> Home </h1>
